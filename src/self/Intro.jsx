7b3487555d70659cebfde1ab507ef12d3f7329d0
OmiSingh04 <omisinghdbz@gmail.com>
2024-10-08 00:05:46 +0530

    intro

---
+++ src/self/Intro.jsx
@@ -0,0 +1,13 @@
+let Intro = () => {
+    return (
+        <>
+            <p style={{fontSize : '70px'}} className="mt-5">Hello, Stranger.</p>
+            {/* <p style={{fontSize : '70px'}} className="mt-5">Hello, Stranger.</p> */}
+            <p className="mt-5 fs-1">I showcase my stuff  here.</p>
+
+            <p className="mt-5 fs-4">(In development, both the stuff and this page.)</p>
+
+        </>
+    )
+}
+export default Intro;
